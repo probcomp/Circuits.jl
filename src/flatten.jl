7e6516d50c2d550576135a9a27b638069e6c8ba0
McCoy Becker <mccoybecker@gmail.com>
2021-06-30 11:12:46 -0700

    Push flatten update.

--- src/flatten.jl
+++ src/flatten.jl
@@ -31,6 +31,8 @@ Base.iterate(out::Output) = (out, nothing)
 Base.iterate(out::Output, n::Nothing) = n
 Base.iterate(inp::CompIn) = (inp, nothing)
 Base.iterate(inp::CompIn, n::Nothing) = n
+Base.iterate(out::CompOut) = (out, nothing)
+Base.iterate(out::CompOut, n::Nothing) = n
 
 second(t::Tuple) = t[2 : end]
 
@@ -71,90 +73,84 @@ function new_names(src::CompIn,
         out_to_idx,
         subcomp_mappings)
 
-    # Dumb debug.
-    sleep(0.3)
-    display(src)
-
-    # Inverse map for outputs.
-    d = Dict(v => k for (k, v) in subcomp_ms[2]) 
     comp_name = src.comp_name
     in_name = src.in_name
     subcomp = flt_subs[comp_name]
     subcomp_ms = subcomp_mappings[comp_name]
+    
+    # Inverse map for outputs.
+    d = Dict(v => k for (k, v) in subcomp_ms[2]) 
+
     indexed = []
 
     # Generate list of receivers from `src` and the subcomp
     # which `src` enters into.
     if subcomp isa PrimitiveComponent
-        rec = map(Output, Base.keys(Circuits.outputs(subcomp)))
+        index = subsubcomp_name_to_idx[(comp_name, comp_name)]
+        push!(indexed, CompIn(index, in_name))
+        return Iterators.flatten(indexed)
     else
+        
         inmap = subcomp_ms[1]
         ind = inmap[in_name]
         rec = receivers(subcomp, Input(ind))
-    end
 
+        for r in rec
 
-    for r in rec
+            # Two cases:
 
-        # Two cases:
+            # (1) CompIn -- we should check if the subcomp is a 
+            # PrimitiveComponent. If it is, we construct the right CompIn
+            # to reference the index + plus the internal in_name.
+            # If it's not Primitive, it should already be the right
+            # index.
 
-        # (1) CompIn -- we should check if the subcomp is a 
-        # PrimitiveComponent. If it is, we construct the right CompIn
-        # to reference the index + plus the internal in_name.
-        # If it's not Primitive, it should already be the right
-        # index.
-
-        if r isa CompIn
-            if subcomp isa PrimitiveComponent
-
-                # Note that this should handle re-naming as we move
-                # up from leaves of DFS automatically...
-                subcomp_name = subsubcomp_name_to_idx[(src.comp_name, r.in_name)]
-                push!(indexed, CompIn(subcomp_name, r.in_name))
-            else
-
+            if r isa CompIn
+                
                 # r should already correspond to a
-                # primitive index.
-                push!(indexed, r) 
-            end
+                # primitive index which refers to
+                # (1) r.comp_name == index
+                # (2) r.in_name   == primitive component input
+                subcompname = subsubcomp_name_to_idx[(comp_name, r.comp_name)]
+                new = CompIn(subcompname, r.in_name)
+                push!(indexed, new)
 
-            # (2) Output -- this is the Output from a subcomponent
-            # which means we need to get the receivers from
-            # that Output in `c` -- the current component scope.
-            # Now, if subcomp is a PrimitiveComponent -- we can use
-            # the receiver id directly -- because this name hasn't 
-            # been mapped to an index yet.
-            # If the subcomp is not a PrimitiveComponent,
-            # we need to use the output map from `subcomp_mappings`
-            # to figure out what the correct name is. Below, this is
-            # `d`.
+                # (2) Output -- this is the Output from a subcomponent
+                # which means we need to get the receivers from
+                # that Output in `c` -- the current component scope.
+                # 
+                # Now, if subcomp is a PrimitiveComponent -- we can use
+                # the receiver id `r.id` directly 
+                # -- because this name hasn't been mapped to an index yet.
+                # 
+                # If the subcomp is not a PrimitiveComponent,
+                # we need to use the output map from `subcomp_mappings`
+                # to figure out what the correct name is. Below, this is
+                # `d`.
 
-        elseif r isa Output
+            elseif r isa Output
 
-            # If we're not working with a primitive component,
-            # we should have already created an integer index.
-            
-            if !(subcomp isa PrimitiveComponent)
+                # If we're not working with a primitive component,
+                # we should have already created an integer index.
+
                 key = d[r.id]
-            else
-                key = r.id
-            end
 
-            # Now we get all the receivers in the current
-            # component scope. We iterate over receivers 
-            # and bail out if we detect a cycle 
-            # (e.g. a receiver is the same as the current
-            # `src::CompIn`)
-            # Otherwise, we call `new_names` and recurse.
-            
-            rec_in_c = receivers(c, CompOut(comp_name, key))
-            for t in rec_in_c
+                # Now we get all the receivers in the current
+                # component scope. We iterate over receivers 
+                # and bail out if we detect a cycle 
+                # (e.g. a receiver is the same as the current
+                # `src::CompIn`)
+                # Otherwise, we call `new_names` and recurse.
 
-                # We need to bail out of cycles...
-                if t == src
-                    subcomp_name = subsubcomp_name_to_idx[(src.comp_name, t.comp_name)]
-                    push!(indexed, CompIn(subcomp_name, in_name))
-                else
+                rec_in_c = receivers(c, CompOut(comp_name, key))
+
+                for t in rec_in_c
+
+                    # We need to bail out of cycles...
+                    #if t == src
+                    #    subcomp_name = subsubcomp_name_to_idx[(src.comp_name, t.comp_name)]
+                    #    push!(indexed, CompIn(subcomp_name, in_name))
+                    #else
                     append!(indexed, new_names(t,
                                                c,
                                                flt_subs,
@@ -163,6 +159,7 @@ function new_names(src::CompIn,
                                                out_to_idx,
                                                subcomp_mappings)
                            )
+                    #end
                 end
             end
         end
@@ -177,25 +174,28 @@ function new_names(dest::CompOut,
         in_to_idx, 
         out_to_idx,
         subcomp_mappings)
+
     comp_name = dest.comp_name
     out_name = dest.out_name
     subcomp = flt_subs[comp_name]
     subcomp_ms = subcomp_mappings[comp_name]
+    indexed = []
+
     if subcomp isa PrimitiveComponent
-        inp = ()
+        index = subsubcomp_name_to_idx[(comp_name, comp_name)]
+        return (CompOut(index, out_name), )
     else
         o_map = subcomp_ms[2]
         k = o_map[out_name]
         inp = inputters(subcomp, Output(k))
-    end
-    indexed = map(inp) do r
-        if r isa CompOut
-            subcomp_name = subsubcomp_name_to_idx[(dest.comp_name, r.comp_name)]
-            CompOut(subcomp_name, r.out_name)
-        else
-            ()
+        for tg in inp
+            if tg isa CompOut
+                subcomp_name = subsubcomp_name_to_idx[(dest.comp_name, tg.comp_name)]
+                push!(indexed, CompOut(subcomp_name, tg.out_name))
+            end
         end
     end
+
     return Iterators.flatten(indexed)
 end
 
@@ -208,24 +208,26 @@ function flat_subsub(v)
 end
 
 function _flatten(c::CompositeComponent)
+
     flt_subs_and_mappings = map(_flatten, c.subcomponents)
     flt_subs = map(first, flt_subs_and_mappings)
     subcomp_mappings = map(second, flt_subs_and_mappings)
-    # each element of subcomp_mappings is `input_map, output_map` giving the name
     idx_to_in = inputs(c) |> keys_deep |> collect
     idx_to_out = outputs(c) |> keys_deep |> collect
     in_to_idx = Dict(v => k for (k, v) in enumerate(idx_to_in))
     out_to_idx = Dict(v => k for (k, v) in enumerate(idx_to_out))
     idx_to_subsubcomp_name = []
-    for (name, subcomp) in pairs(c.subcomponents)
+
+    for (name, subcomp) in pairs(flt_subs)
         if subcomp isa CompositeComponent 
-            for (innername, _) in pairs(subcomp.subcomponents)
-                push!(idx_to_subsubcomp_name, (name, innername))
+            for (inner_index, _) in pairs(subcomp.subcomponents)
+                push!(idx_to_subsubcomp_name, (name, inner_index))
             end
         else
             push!(idx_to_subsubcomp_name, (name, name))
         end
     end
+
     subsubcomp_name_to_idx = Dict(v => k for (k, v) 
                                   in enumerate(idx_to_subsubcomp_name))
     new_edges = []
@@ -251,8 +253,6 @@ function _flatten(c::CompositeComponent)
         end
     end
 
-    @info "Building new flat component..."
-    display(new_edges)
     new_comp = CompositeComponent(
                                   IndexedValues([inputs(c)[name] for name in idx_to_in]),
                                   IndexedValues([outputs(c)[name] for name in idx_to_out]),
@@ -262,6 +262,5 @@ function _flatten(c::CompositeComponent)
 end
 
 function flatten(c::CompositeComponent)
-    new_comp, in_to_idx, out_to_idx = _flatten(c)
-    return new_comp
+    return _flatten(c)
 end
