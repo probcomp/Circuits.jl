1698b3c5ff98fdf88cbed29ebfb46d660d20ecf3
George Matheos <georgematheos@gmail.com>
2021-08-03 17:10:40 -0400

    include inputs and outputs in io-node transform

--- src/inputs_outputs_to_components.jl
+++ src/inputs_outputs_to_components.jl
@@ -18,16 +18,16 @@ of each of the input/output value nodes from the original component.
 """
 inputs_outputs_to_components(c::CompositeComponent) =
     CompositeComponent(
-        CompositeValue(()), CompositeValue(()),
+        inputs(c), outputs(c),
         (;
             original_subcomponents=ComponentGroup(c.subcomponents),
             input_nodes=nodes_for_value(inputs(c)),
             output_nodes=nodes_for_value(outputs(c))
         ),
-        [
-            update_original_edge(src, dst)
-            for (src, dst) in get_edges(c)
-        ]
+        (
+            (update_original_edge(src, dst) for (src, dst) in get_edges(c))...,
+            io_node_input_edges(c)..., io_node_output_edges(c)...
+        )
     )
 
 nodes_for_value(v::Value) = InputOutputComponent(v)
@@ -41,3 +41,10 @@ update_src(n::CompOut) = CompOut(:original_subcomponents, n.comp_name => valname
 update_dst(n::Output) = CompIn(:output_nodes, nest(valname(n), :in))
 update_dst(::Union{Input, CompOut}) = error() # should never happen
 update_dst(n::CompIn) = CompIn(:original_subcomponents, n.comp_name => valname(n))
+
+io_node_input_edges(c) = (
+    Input(i) => CompIn(:input_nodes => i, :in) for i in keys_deep(inputs(c))
+)
+io_node_output_edges(c) = (
+    CompOut(:output_nodes => o, :out) => Output(o) for o in keys_deep(outputs(c))
+)
